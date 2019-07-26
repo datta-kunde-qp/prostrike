154aea6e6a027f2b8d580d3dd6bbca5330e2ad54
Nehal-29 <nehal.sanklecha@questionpro.com>
2019-07-26 12:01:37 +0530

    home screen

--- testchatapp/App.js
+++ testchatapp/App.js
@@ -7,44 +7,29 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import io from "socket.io-client";
-import Canvas from 'react-native-canvas';
+import { StyleSheet, View, ImageBackground,TouchableOpacity, Text } from "react-native";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.canvasRef = React.createRef();
     }
-
-    componentDidMount() {
-        this.socket = io("http://192.168.4.59:3000");
-
-        const canvas = this.canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.fillStyle = 'red';
-        context.fillRect(50, 100, 50, 550);
-
-    }
-
-    addNewPlayer() {
-        this.socket.emit('newPlayer');
-    }
-
-    drawPlayer() {
-        const ctx = canvas.getContext('2d');
-        const drawPlayer = (player) => {
-            ctx.beginPath();
-            ctx.rect(player.x, player.y, player.width, player.height);
-            ctx.fillStyle = '#0095DD';
-            ctx.fill();
-            ctx.closePath();
-        };
-    }
-
 
     render() {
-        return (<Canvas style={styles.container} ref={this.canvasRef}/>);
+        return (<View style={styles.container}>
+            <ImageBackground source={require('./images/homepage.png')} resizeMode='stretch' style={styles.image}/>
+            <View style={[styles.buttonStyle,{bottom: 80, marginBottom:20}]}>
+                <TouchableOpacity style={{flex:1}}>
+                <Text style={{fontSize: 25,
+                    textAlignVertical: 'center'}}>Join Room</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonStyle}>
+                <TouchableOpacity style={{flex:1}}>
+                    <Text style={{fontSize: 25,
+                        textAlignVertical: 'center'}}>Create Room</Text>
+                </TouchableOpacity>
+            </View>
+        </View>);
     }
 }
 
@@ -52,7 +37,17 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "grey",
-        margin:20,
-        width:380
+    },
+    image: {width:'100%', flex:1},
+    buttonStyle:{
+        width:'90%',
+        height:50,
+        backgroundColor: 'grey',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 40,
+        marginHorizontal:10
     }
+
 });
