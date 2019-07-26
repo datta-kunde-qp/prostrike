1c44801fe5b0d2f0a9e5561daa92d7f6e87f97a8
Nehal-29 <nehal.sanklecha@questionpro.com>
2019-07-26 10:30:27 +0530

    initial

---
+++ testchatapp/App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import io from "socket.io-client";
+import Canvas from 'react-native-canvas';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.canvasRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.socket = io("http://192.168.0.106:3000");
+
+        const canvas = this.canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.fillStyle = 'red';
+        context.fillRect(50, 100, 50, 550);
+
+    }
+
+    addNewPlayer() {
+        this.socket.emit('newPlayer');
+    }
+
+    drawPlayer() {
+        const ctx = canvas.getContext('2d');
+        const drawPlayer = (player) => {
+            ctx.beginPath();
+            ctx.rect(player.x, player.y, player.width, player.height);
+            ctx.fillStyle = '#0095DD';
+            ctx.fill();
+            ctx.closePath();
+        };
+    }
+
+
+    render() {
+        return (<Canvas style={styles.container} ref={this.canvasRef}/>);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "grey",
+        margin:20,
+        width:380
+    }
+});
---
+++ testchatapp/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 3000;
+
+const gameState = {
+    players
+}
+
+
+io.on("connection", socket => {
+    console.log("a user connected");
+    socket.on("chat message", msg => {
+        console.log(msg);
+        io.emit("chat message", msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        delete gameState.players[socket.id]
+    });
+    socket.on('newPlayer', () => {
+        gameState.players[socket.id] = {
+            x: 250,
+            y: 250,
+            width: 25,
+            height: 25
+        }
+    })
+    setInterval(() => {
+        io.sockets.emit('state', gameState);
+    }, 1000 / 60);Q23E
+});
+
+
+
+server.listen(port, () => console.log("server running on port :" + port));
